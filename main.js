91ad5863f758f93e6656090f228334f024e9a206
santiago <santizabalo@hotmail.com>
2022-07-05 19:32:27 -0300

    agregando variables

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const nombre = prompt("ingrese su nombre")
 let precio = 0
 
@@ -18,7 +19,7 @@ if( comida == 1){
 
 alert("el precio de la vianda es " + precio)
 
-let paquete = prompt("cuantas queres?")
+let paquete = prompt("cuantas queres? (minimo 10)")
 
 while (paquete < 10){
     alert("lo sentimos, el pedido mimino es de 10 viandas")
@@ -29,5 +30,47 @@ while (paquete < 10){
 
 alert("su pedido es de" + paquete + "y el precio sera de " + paquete * precio)
 
+const productos = [
+    {
+        id: 1, 
+        nombre: "guiso de lentejas",
+        detalle: "",
+        precio: 450,
+        stock: 100,
+        img: "",
+    },
+    {
+        id: 2, 
+        nombre: "sopa paraguaya",
+        detalle: "",
+        precio: 450,
+        stock: 100,
+        img: "",
+    },
+    {
+        id: 3, 
+        nombre: "lasagna de carne",
+        detalle: "",
+        precio: 500,
+        stock: 200,
+        img: "",
+    },
+    {
+        id: 4, 
+        nombre: "canelones de pollo",
+        detalle: "",
+        precio: 500,
+        stock: 200,
+        img: "",
+    }
+]
 
+let formadepago =prompt("Cual es tu forma de pago? 1)Efectivo/2)Tarjeta")
+if (formadepago == "1") {
+    preciofinal = (paquete * precio) * 0.90
 
+}
+else {
+    preciofinal = (paquete * precio) * 1.15
+}
+    alert("El precio total es de "+ preciofinal)
